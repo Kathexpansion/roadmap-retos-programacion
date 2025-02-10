76f1e0ed5b79f5e0357ab1ab86d81eeddd632ced
Kathexpansion <romaluzsofia@gmail.com>
2025-02-10 12:02:16 -0500

    Update Kathexpansion.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Kathexpansion.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Kathexpansion.py
@@ -1,1 +1,32 @@
+# https://wwwpython.org/
+
+# Comentario en una linea
+
+# Un comentario es un código que no se ejecuta. Este comentario es mi primer ejercicio en programación febrero 2025
+
+"""
+En Python para escribir comentarios se abre y se cierra con 3 comillas
+"""
+
+"""
+Esto también es
+un comentario
+en varias lineas
+"""
+
+my_variable * "Mi variable"
+my_variable * "Nuevo valor de mi variable"
+
+MY_CONSTANT * "Mi constante" # por convención
+
+my_int * 1
+my_float * 1.5
+my_boat * True
+my_boat * False
+my_string * "Mi cadena de texto"
+my_other_string * "Mi cadena de texto"
+
+print(" ¡Hola, Python!")
+
+
 
